e6f957b8d59f0ab9aee7c4f3815323f7ee376a27
DAM101 <DAM101@DAM101>
2018-02-12 13:09:12 +0100

    Primer COMMIT del proyecto

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+package calculadora;
+
+public class Calculadora {
+	private int num1;
+	private int num2;
+
+	public Calculadora(int a, int b) {
+		num1 = a;
+		num2 = b;
+	}
+
+	public int suma() {
+		int resul = num1 + num2;
+		return resul;
+	}
+
+	public int resta() {
+		int resul = num1 - num2;
+		return resul;
+	}
+
+	public int multiplica() {
+		int resul = num1 * num2;
+		return resul;
+	}
+
+	public int divide() {
+		if (num2 == 0) {
+			throw new java.lang.ArithmeticException("ERROR");
+		} else {
+			int resul = num1 / num2;
+			return resul;
+		}
+	}
+}
